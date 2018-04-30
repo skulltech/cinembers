3b6dd22c6c330cc6e465f6c2ff3a16bd5176ced9
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-05-01 03:37:31 +0530

    Add get_frames function

--- disassembler.py
+++ disassembler.py
@@ -1,18 +1,16 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
 
-cap = cv2.VideoCapture('/home/sumit/Downloads/IllustriousKindheartedGordonsetter.mp4')
-# cap = cv2.VideoCapture(0)
+def get_frames(name, gray=False):
+	cap = cv.VideoCapture(name)
+	
+	while True:
+		ret, frame = cap.read()
+		if not ret:
+			break
+		if gray:
+			frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+		yield frame
 
-while True:
-	ret, frame = cap.read()
-	print(ret)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	cv2.imshow('frame', gray)
-
-	if cv2.waitKey(1) & 0xFF==ord('q'):
-		break
-
-cap.release()
-cv2.destroyAllWindows()
+	cap.release()
