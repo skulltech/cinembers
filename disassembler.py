2cad6ef429ad48fb3bf2aa0995633d5e9ebcea1d
Sumit Ghosh <sumit@sumit-ghosh.com>
2018-05-01 04:35:17 +0530

    Write Video class properly.

--- disassembler.py
+++ disassembler.py
@@ -1,16 +1,44 @@
 import numpy as np
 import cv2 as cv
+from contextlib import contextmanager
 
 
-def get_frames(name, gray=False):
-	cap = cv.VideoCapture(name)
-	
-	while True:
-		ret, frame = cap.read()
-		if not ret:
-			break
-		if gray:
-			frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		yield frame
 
-	cap.release()
+class Video:
+	def __init__(self, name):
+		self.cap = cv.VideoCapture(name)
+		self.gray = False
+
+	def frames(self):
+		while True:
+			ret, frame = self.cap.read()
+			if not ret:
+				break
+			if self.gray:
+				frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+			yield frame
+
+	def __pairs(self):
+		frames = self.frames()
+		first = next(frames)
+		for frame in frames:
+			yield first, frame
+			first = frame
+
+	def SAD(self):
+		for pair in self.__pairs():
+			yield cv.absdiff(pair[0], pair[1]).sum()
+
+	def norm(self):
+		for pair in self.__pairs():
+			yield cv.norm(pair[0], pair[1], cv.L1_NORM).sum()
+
+	def close(self):
+		self.cap.release()
+
+
+@contextmanager
+def video(name):
+	vid = Video(name)
+	yield vid
+	vid.close()
